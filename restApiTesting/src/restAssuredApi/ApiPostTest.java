cdec446a76add3673262e3d62f63357261d5e66e
CGN04 <CGN04@T4ublrlpusr093.telematics.com>
2019-01-08 13:25:30 +0530

    recent changes

--- restApiTesting/src/restAssuredApi/ApiPostTest.java
+++ restApiTesting/src/restAssuredApi/ApiPostTest.java
@@ -15,41 +15,22 @@ public class ApiPostTest {
 	
 	@Test(dataProvider="dataset",invocationCount=5)
 	public void postTest(String requestParams) throws IOException, ParseException, InterruptedException {
-		
 		RestAssured.baseURI ="http://104.211.178.63/TelematicsRESTService/services/ServiceProcess/DriverCreation";
-
 		RequestSpecification request = RestAssured.given();
-
 		request.headers(HeaderFunc.headersto("test"));
-
-
 		request.body(BodyJson.sendJsonBody(requestParams,"test").toJSONString());
-
 		Response response = request.post("");
-
 		int statusCode = response.getStatusCode();
-
 		String statusLine=response.getStatusLine();
-
 		System.out.println("The status code recieved: " + statusCode);
-
 		System.out.println("The status line recieved: " + statusLine);
-
 		System.out.println("Response body: " + response.body().asString());
-		
 		Thread.sleep(3000);
-		
 		Assert.assertEquals(statusCode, 200);
 	}
-	
 	@DataProvider
 	public static Object[][] dataset() throws Exception{
-
 		Object[][] datas=ExelData.getTableArray(".\\data\\testdata.xls", "jsonBody");
-
 		return datas;
-
 	}
-
-
 }
--- restApiTesting/src/restAssuredApi/Kafka.java
+++ restApiTesting/src/restAssuredApi/Kafka.java
@@ -15,40 +15,23 @@ import io.restassured.specification.RequestSpecification;
 
 public class Kafka {
 	public class ApiPostTest {
-		
 		@Test(dataProvider="data")
 		public void postTest(String requestParams) throws IOException, ParseException {
-
 			RestAssured.baseURI ="http://172.16.2.193:9091/kafka/pushAssetDataKafka/";
-
 			RequestSpecification request = RestAssured.given();
-
 			request.headers(HeaderFunc.headersto("te"));
-
-
 			request.body(BodyJson.sendJsonBody(requestParams,"t").toJSONString());
-
 			Response response = request.post("");
-
 			int statusCode = response.getStatusCode();
-
 			String statusLine=response.getStatusLine();
-
 			System.out.println("The status code recieved: " + statusCode);
-
 			System.out.println("The status line recieved: " + statusLine);
-
 			System.out.println("Response body: " + response.body().asString());
 		}
-		
 		@DataProvider
 		public Object[][] data() throws Exception{
-
 			Object[][] datas=ExelData.getTableArray(".\\data\\input.xls", "input");
-
 			return datas;
-
 		}
-
 	}
 }
